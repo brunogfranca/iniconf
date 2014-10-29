4252d0c758ae8c71c4c883a50b8369730524d0c9
Bruno Gomes França <bruno.franca@upessencia.com.br>
2014-10-29 11:56:02 -0200

    docs

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='iniconf',
     version='0.1.0',
     description='INI Config Creating/Parsing Tool',
-    long_description=open('README').read(),
+    long_description=open('README.md').read(),
     url='https://github.com/brunogfranca/iniconf',
     author='Bruno Franca',
     author_email='bgfranca@gmail.com',
