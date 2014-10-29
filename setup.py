ae8a834f427ef7781344df2ef44cbf3fdb9dba42
Bruno Gomes França <bruno.franca@upessencia.com.br>
2014-10-29 13:57:27 -0200

    pypi publishing fixes

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-from ez_setup import use_setuptools
-use_setuptools()
 from setuptools import setup, find_packages
 
 
 setup(
     name='iniconf',
-    version='0.1.0',
+    version='0.1.1',
     description='INI Config Creating/Parsing Tool',
     long_description=open('README.md').read(),
     url='https://github.com/brunogfranca/iniconf',
