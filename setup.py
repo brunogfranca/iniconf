009fe3369870a2dd97aea2597e4a54f48dba2b3d
Bruno Gomes França <bruno.franca@upessencia.com.br>
2014-10-30 12:03:57 -0200

    bumping version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='iniconf',
-    version='0.1.1',
+    version='0.1.2',
     description='INI Config Creating/Parsing Tool',
     long_description=open('README.md').read(),
     url='https://github.com/brunogfranca/iniconf',
