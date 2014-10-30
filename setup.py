c1a7e5a953e109a0502726aa1771832b9eb17246
Bruno Gomes França <bruno.franca@upessencia.com.br>
2014-10-30 12:14:24 -0200

    updating setup.py to include the license

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='iniconf',
-    version='0.1.4',
+    version='0.1.5',
     description='INI Config Creating/Parsing Tool',
     long_description=open('README.md').read(),
     url='https://github.com/brunogfranca/iniconf',
     author='Bruno Franca',
     author_email='bgfranca@gmail.com',
-    license='LICENSE.txt',
+    license=open('LICENSE.txt').read(),
     packages=find_packages()
 )
